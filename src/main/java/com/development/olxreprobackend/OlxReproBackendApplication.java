0c3b5f273c5e7b6acb13f9c9e045fc15f84214d2
beatrizcunharo <beatrizcunharo@gmail.com>
2023-04-04 23:57:05 -0300

    feat: Criação do projeto

---
+++ src/main/java/com/development/olxreprobackend/OlxReproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.development.olxreprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxReproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxReproBackendApplication.class, args);
+	}
+
+}
