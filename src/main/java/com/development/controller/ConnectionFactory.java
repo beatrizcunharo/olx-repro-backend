c40c9805bafa85d2f90d70c897a190b6d5301b13
beatrizcunharo <beatrizcunharo@gmail.com>
2023-04-05 23:46:00 -0300

    refactor: Pequeno ajuste apenas para descrever o que é necessário na conexão com o banco

--- src/main/java/com/development/controller/ConnectionFactory.java
+++ src/main/java/com/development/controller/ConnectionFactory.java
@@ -7,14 +7,15 @@ import java.sql.SQLException;
 // CONEXÃO COM O BANCO DE DADOS
 public class ConnectionFactory {
 	public Connection connection () throws ClassNotFoundException {
+		// VERIFICAR PARÂMETROS TODA VEZ QUE FOR RODAR O BANCO
 		String url_db = "jdbc:mysql://localhost:3306/db_olx";
         String user_db = "root";
-        String password_db = "siprogram1165"; // TROCAR PARA A SUA SENHA TODA VEZ QUE UTILIZAR O BANCO
+        String password_db = "siprogram1165"; 
         
         Connection db_connection = null;
         try {
         	db_connection = DriverManager.getConnection(url_db, user_db, password_db);
-            System.out.println("Banco conectado");
+            System.out.println("Banco db_olx conectado.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
